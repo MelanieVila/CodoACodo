130f4001face74c4ed52081aa8b062942acc777c
MelanieVila <melanievila19@hotmail.com>
2022-10-06 19:24:10 -0300

    06102022

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let url = "./js/tipoagua.json"
+let datos = [];
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => { console.log(data)
+        datos = data.datos
+        console.log(datos)
+
+        let cad = `<div class="container">`
+
+        for (tipoagua of datos) {
+            cad += `
+        <div class="tarjeta">
+                <img src="${tipoagua.img}" alt="${tipoagua.nombre}">
+                    <div class="cuerpo">
+                        <h4>Nombre:${tipoagua.name}</h4>
+                    </div>
+                </div>
+        `
+        }
+
+        cad += `</div>`
+
+        console.log(cad)
+
+        document.getElementById("tipoagua").innerHTML = cad
+
+    }
+    );
