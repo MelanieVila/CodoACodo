b26e5d6b577ab64109149eec64cfedef98558b60
mikorusso <micaelarussozamora@gmail.com>
2022-10-21 17:44:28 -0300

    pequeño error corregido

--- js/formulario.js
+++ js/formulario.js
@@ -68,11 +68,3 @@ formulario.addEventListener('submit', (e) => {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
 	}
 });
-
-desplazarArriba.addEventListener("click", () => {
-	window.scrollTo({
-	  top: 0,
-	  left: 0,
-	  behavior: "smooth",
-	});
-  });
